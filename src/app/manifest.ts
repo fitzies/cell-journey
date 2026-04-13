ff3c21494f69c33bfe01502aec18a04ee710d56b
fitzies <olicrypt@gmail.com>
2026-04-13 18:24:24 +0800

    chore: set PWA theme_color to white

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -9,7 +9,7 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: "/",
     display: "standalone",
     background_color: "#ffffff",
-    theme_color: "#5B4FCF",
+    theme_color: "#ffffff",
     icons: [
       {
         src: "/icon-192.png",
